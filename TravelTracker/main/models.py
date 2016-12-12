29d16fb8de909a0dc127c3e7db1624cbb7411ee5
Chen Su <martinsuchen3557@gmail.com>
2016-12-11 23:40:41 -0500

    Add User to Location model.

--- TravelTracker/main/models.py
+++ TravelTracker/main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # once added model, go to Django shell to migrate, then create objects
 class Location(models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(max_length=100)
     predators = models.CharField(max_length=100)
     num_restaurants = models.IntegerField()
